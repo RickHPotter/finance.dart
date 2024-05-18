30c01ec22c62c7195dae688fe225b8d0fedacacd
RickHPotter <luisfla55@hotmail.com>
2024-05-17 22:08:50 -0300

    init: initial commit
    Set up Flutter project
    Set up Folder Structure

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'src/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Finance',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
---
+++ lib/src/models/user_model.dart
@@ -0,0 +1,25 @@
+class User {
+  String? name;
+  String? email;
+  String? password;
+  String? token;
+
+  User(
+      {this.name, this.email, this.password, this.token});
+
+  User.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    email = json['email'];
+    password = json['password'];
+    token = json['token'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['name'] = name;
+    data['email'] = email;
+    data['password'] = password;
+    data['token'] = token;
+    return data;
+  }
+}
